4060bfcb95f794a06a51aad6bfd60bb109953d37
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-10 12:19:05 +0000

    added comments to both python files

--- checker.py
+++ checker.py
@@ -1,12 +1,13 @@
-
+# Creating our Checker class to hold the functionality we want to test
 class Checker:
-
+    # Creating our clean division method which checks if one value is cleanly divisible by the other
     def clean_div(self, val1, val2):
         if val1 % val2 == 0:
-            return True
+            return True  # If there is no remainder after dividing then we should return True
         else:
-            return False
+            return False  # Otherwise we should return false
 
+    # Creating our positive method which checks if a number is positive or not
     def positive(self, val1):
         if val1 > 0:
             return True
--- test_checker.py
+++ test_checker.py
@@ -1,14 +1,20 @@
+# Importing our testing modules and the class we are testing
 from checker import Checker
 import unittest
 import pytest
 
 
+# Creating our testing class that will inherit from the unittest Testcase framework
 class TestChecker(unittest.TestCase):
+    # Instantiating an object of the Checker class
     test = Checker()
 
+    # Our test to check if the correct boolean is returned when a number is and isn't cleanly divisible by the other
     def test_clean_div(self):
         self.assertEqual(self.test.clean_div(10, 5), True)
+        self.assertEqual(self.test.clean_div(10, 4), False)
 
+    # Our test to check if the correct boolean is returned when a positive and negative number are provided
     def test_positive(self):
         self.assertEqual(self.test.positive(-5), False)
         self.assertEqual(self.test.positive(8), True)
